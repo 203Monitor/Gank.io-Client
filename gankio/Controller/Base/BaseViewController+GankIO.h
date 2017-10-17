4bfae7cfba93a303bc5cd565fcf66b1ae0837cd3
203monitor <930502900@qq.com>
2017-10-17 23:16:27 +0800

    post

---
+++ gankio/Controller/Base/BaseViewController+GankIO.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController+GankIO.h
+//  gankio
+//
+//  Created by 武国斌 on 2017/10/17.
+//  Copyright © 2017年 武国斌. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController (GankIO)
+
+@end
---
+++ gankio/Controller/Base/BaseViewController+GankIO.m
@@ -0,0 +1,13 @@
+//
+//  BaseViewController+GankIO.m
+//  gankio
+//
+//  Created by 武国斌 on 2017/10/17.
+//  Copyright © 2017年 武国斌. All rights reserved.
+//
+
+#import "BaseViewController+GankIO.h"
+
+@implementation BaseViewController (GankIO)
+
+@end
